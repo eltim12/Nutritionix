8603256c83ff4f3046cae919081205e6027ae900
eltim12 <eltim2000@gmail.com>
2019-04-04 17:48:44 +0700

    first commit

---
+++ server/app.js
@@ -0,0 +1,31 @@
+require("dotenv").config()
+
+const express = require("express")
+const cors = require("cors")
+const app = express()
+const port = 3000
+
+const mongoose = require("mongoose")
+mongoose.set("useFindAndModify", false)
+
+mongoose.connect('mongodb://localhost:27017/fancyTodo', { useNewUrlParser: true })
+
+app.use(express.urlencoded({ extended: false }))
+
+app.use(express.json())
+
+app.use(cors())
+
+const zomatoRoutes = require('./routes/zomato')
+const googleMapsRoutes = require('./routes/googleMaps')
+const nutritionRoutes = require('./routes/nutrition')
+const userRoutes = require('./routes/users')
+
+app.use('/zomatos', zomatoRoutes)
+app.use('/googleMaps', googleMapsRoutes)
+app.use('/nitritionRoutes', nutritionRoutes)
+app.use('/users', userRoutes)
+
+
+module.exports = app
+app.listen(port, () => console.log("listening on port" + port))
